aa1b13de1c1e4bab89b93d51e94d6b237f956a7b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-14 19:15:44 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,10 +19,9 @@ class StudentsController < ApplicationController
   #	@student.save
 	#	redirect_to student_path(@student)
     @student = Student.create(
-      {
+  
         first_name: params[:first_name],
         last_name: params[:last_name]
-      }
     )
     redirect_to student_path(@student)
   end
